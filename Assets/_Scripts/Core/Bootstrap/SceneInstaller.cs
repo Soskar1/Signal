6dde3eaca276dffcb4c195a11d5e361f8413bb6f
oskar <oskar.vistorskis@gmail.com>
2026-04-18 11:40:37 +0300

    DI container

---
+++ Assets/_Scripts/Core/Bootstrap/SceneInstaller.cs
@@ -0,0 +1,13 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace Signal.Core
+{
+    public class SceneInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            
+        }
+    }
+}
