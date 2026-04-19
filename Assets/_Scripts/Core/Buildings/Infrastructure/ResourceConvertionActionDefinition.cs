72b1258a09a7daf5eb1d015012b33617fb7b583d
oskar <oskar.vistorskis@gmail.com>
2026-04-19 20:51:42 +0300

    dasf

---
+++ Assets/_Scripts/Core/Buildings/Infrastructure/ResourceConvertionActionDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Signal.Core.Economy;
+using UnityEngine;
+
+namespace Signal.Core.Buildings.Infrastructure
+{
+    [CreateAssetMenu(fileName = "IncreaseResourceAction", menuName = "Signal/Buildings/Actions/Increase Resource")]
+    internal class ResourceConvertionActionDefinition : BuildingActionDefinition
+    {
+        [SerializeField] private ResourceId _from;
+        [SerializeField] private ResourceId _to;
+        [SerializeField] private int _amount;
+
+        public ResourceId From => _from;
+        public ResourceId To => _to;
+        public int Amount => _amount;
+    }
+}
